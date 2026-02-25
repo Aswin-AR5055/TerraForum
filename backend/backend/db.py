61e501c4bb3708f2b7b37fd64fa8af4d5d7ff613
Aswin-AR5055 <aswinraj2003@gmail.com>
2026-02-25 13:20:58 +0530

    using fallback for db port

--- backend/backend/db.py
+++ backend/backend/db.py
@@ -5,5 +5,5 @@ def db_connect():
     mongoengine.connect(
         db=os.getenv("DB_NAME"),
         host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
+        port=int(os.getenv("DB_PORT", 27017)),
     )
